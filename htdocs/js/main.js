173194fa8bd22e58c66a2aa449a6bc370ec4d064
山本海実 <ymmt89@gmail.com>
2015-06-21 22:34:55 +0900

    BGM 一時停止　追加

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -1,3 +1,20 @@
+//load
+
+// sound
+var getSound = document.getElementById("is--sound");
+var getStart = document.getElementById("soundStart");
+var getStop = document.getElementById("soundStop");
+function soundStop(){
+  getSound.pause();
+  getStart.style.display = "block";
+  getStop.style.display = "none";
+}
+function soundStart(){
+  getSound.play();
+  getStop.style.display = "block";
+  getStart.style.display = "none";
+}
+//etc
 ! function (t) {
     t(function () {
         t("a img").each(function () {
@@ -13,7 +30,7 @@
             t(this).attr("src", t(this).attr("src").replace("_ov.", "_of."))
         })
     }), t(function () {
-        var i = ["chara", "sns"];
+        var i = ["chara","sns"];
         t.each(i, function (i, n) {
             t("#" + n).append('<div class="point"><img src="img/' + n + '_ttl_point.png"></div>')
         })
