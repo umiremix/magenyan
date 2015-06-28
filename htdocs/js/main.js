866b7b91eb2bca3a5f4f605767a3b7c7c055da4b
山本海実 <ymmt89@gmail.com>
2015-06-28 15:13:45 +0900

    スマホ　対応　途中

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -1,3 +1,7 @@
+/*--------------------
+common
+----------------------*/
+
 //load
 $(window).load(function(){
     $('#gg0').css({'display':'none'});
@@ -68,10 +72,58 @@ $(function(){
         var $getHref = $(this).find('a').attr('href');
         return $getHref;
     }
-    });
+  });
 });
+// function openCallBack(){
+//   var $getHref = $(this).find('a').attr('href');
+//   console.log($getHref);
+// //       $('.chara_img').animate({
+// //           opacity: 1,
+// //         if(getId === 'koyama'){
+// //           bottom: "-40px",
+// //           left: "-109px"
+// //         } else if(getId === 'musashi'){
+// //           bottom: "-42px",
+// //           left: "-36px"
+// //         } else if(getId === 'myanya'){
+// //           bottom: "-51px",
+// //           left: "-78px"
+// //         } else if(getId === 'momo'){
+// //           bottom: "-42px",
+// //           left: "-34px"
+// //         } else if(getId === 'magenyan'){
+// //           bottom: "-43px",
+// //           left: "-110px"
+// //         } else if(getId === 'taro'){
+// //           bottom: "-48px",
+// //           left: "-53px"
+// //         }
+// }
 
-//sp
-$(function(){
-   $
-});
+/*--------------------
+ tablet & sp
+----------------------*/
+//setting
+var _ua = (function(u){
+  return {
+    Tablet:(u.indexOf("windows") != -1 && u.indexOf("touch") != -1)
+      || u.indexOf("ipad") != -1
+      || (u.indexOf("android") != -1 && u.indexOf("mobile") == -1)
+      || (u.indexOf("firefox") != -1 && u.indexOf("tablet") != -1)
+      || u.indexOf("kindle") != -1
+      || u.indexOf("silk") != -1
+      || u.indexOf("playbook") != -1,
+    Mobile:(u.indexOf("windows") != -1 && u.indexOf("phone") != -1)
+      || u.indexOf("iphone") != -1
+      || u.indexOf("ipod") != -1
+      || (u.indexOf("android") != -1 && u.indexOf("mobile") != -1)
+      || (u.indexOf("firefox") != -1 && u.indexOf("mobile") != -1)
+      || u.indexOf("blackberry") != -1
+  }
+})(window.navigator.userAgent.toLowerCase());
+
+//sound & video
+if(_ua.Mobile || _ua.Mobile){
+  $('#is--sound').remove();
+  $('#is--video').remove();
+}
